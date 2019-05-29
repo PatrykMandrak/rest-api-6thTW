dc4e67e33fcc67fccd2895e421f07f6f030c1ff2
yasha2yashaa <yasha2yashaa@gmail.com>
2019-05-29 09:46:42 +0200

    Added DAO interface

--- src/main/java/com/codecool/restAPI/DAOs/IDAO.java
+++ src/main/java/com/codecool/restAPI/DAOs/IDAO.java
@@ -1,5 +1,18 @@
 package com.codecool.restAPI.DAOs;
 
-public interface IDAO {
+import java.util.List;
 
+public interface IDAO<E> {
+
+    public void create(E e);
+
+    public E getById(E e);
+
+    public E getByname(String name);
+
+    public List<E> getAll();
+
+    public void Update(E e, int id);
+
+    public void delete(E e);
 }
